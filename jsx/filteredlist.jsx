cd991c148256849bf44485eac1f48b6a073f81bc
gongzhen <gongzhengz@gmail.com>
2017-05-07 14:10:31 -0700

    React+Reduct app done.

--- jsx/filteredlist.jsx
+++ jsx/filteredlist.jsx
@@ -1,23 +1,62 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
+const React = require('react');
+const {Component} = require('react')
+const {createStore} = require('redux')
+
+const FRAMEWORKS = ['React', 'Angular', 'Vue', 'Ember']
+
+// ACTION: CREATORS
+function setFilter(by) {
+	return {type:'SET_FILTER', by}
+}
+
+// REDUCER
+const initialState = {
+	filterBy: ''
+}
+
+function reducer(state = initialState, action) {
+	switch(action.type) {
+		case 'SET_FILTER':
+			console.log(`Our filter was ${state.filterBy} byt is now ${action.by}!`)
+			return Object.assign({}, state, {
+				filterBy: action.by
+			})
+		default:
+			return state
+	}
+}
+
+// STORE
+const store = createStore(reducer);
+
+// react-redux
+const mapStateToProps = (state) => {
+	return {
+		filterBy:state.filterBy
+	}
+}
 
 module.exports = class FilterList extends React.Component {
 	constructor(props) {
-	  super(props);
-	  this.state = {
-	  	filterBy: ''
-	  };
+	  super()
+	  // default state
+	  this.state = store.getState()
+	  // function that will execute every time the state changes.
+	  this.unsubscribe = store.subscribe(() => {
+	  	this.setState(store.getState())
+	  })
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe()
 	}
 
 	updateFilter(ev) {
-		console.log(ev.target)
-		console.log(ev.target.value)
-		this.setState({filterBy: ev.target.value})
+		store.dispatch(setFilter(ev.target.value))
 	}
 
 	render() {
 		const {filterBy} = this.state
-		const frameworks = ['React', 'Angular', 'Vue', 'Ember']
 		// simple input box and our list componet
 		const List = ({ items, filterBy }) => {
 			return(
@@ -34,7 +73,7 @@ module.exports = class FilterList extends React.Component {
 		return(
 			<div>
 				<input type = "text" onChange = {(ev) => this.updateFilter(ev) }/>
-				<List items = {frameworks} filterBy = {filterBy} />
+				<List items = {FRAMEWORKS} filterBy = {filterBy} />
 			</div>
 		)
 	}
